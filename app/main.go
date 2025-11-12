9704b53d746362381e8607950dcedfa5562f45b0
Tikaram Mardi <tikarammardi007@gmail.com>
2025-11-12 21:01:38 +0530

    implement command input and error handling in main function

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
@@ -22,9 +23,28 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		cmd := command[:len(command)-1]
+		input := command[:len(command)-1]
+
+		parts := strings.SplitN(input, " ", 2)
+
+		cmd := parts[0]
+		args := parts[1:]
+
+		var firstArg string
+
+		if len(args) != 0 {
+			firstArg = args[0]
+		} else {
+			firstArg = ""
+		}
+
 		if cmd == "exit" {
-			break
+			if firstArg == "0" {
+				os.Exit(0)
+			}
+			if firstArg == "1" {
+				os.Exit(1)
+			}
 		}
 		fmt.Println(cmd + ": command not found")
 
