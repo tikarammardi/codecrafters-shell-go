798879233e6bef22c0fe6e1d230d6292ea8c131c
Tikaram Mardi <tikarammardi007@gmail.com>
2025-11-26 22:37:29 +0530

    refactor command handling and execution logic

--- app/main.go
+++ app/main.go
@@ -15,6 +15,8 @@ var (
 	_ = os.Stdout
 )
 
+var builtIncommandList = []string{"echo", "exit", "type"}
+
 func main() {
 	for {
 		_, err := fmt.Fprint(os.Stdout, "$ ")
@@ -32,46 +34,7 @@ func main() {
 		cmd := parts[0]
 		args := parts[1:]
 
-		var firstArg string
-
-		builtIncommandList := []string{"echo", "exit", "type"}
-
-		if cmd == "exit" {
-
-			if len(args) != 0 {
-				firstArg = args[0]
-			} else {
-				firstArg = ""
-			}
-
-			if firstArg == "0" {
-				os.Exit(0)
-			}
-			if firstArg == "1" {
-				os.Exit(1)
-			}
-			os.Exit(0)
-		} else if cmd == "echo" {
-			fmt.Print(strings.Join(args, " ") + "\n")
-		} else if cmd == "type" {
-			firstArg = args[0]
-			isBuiltInCommandInList := slices.Contains(builtIncommandList, firstArg)
-			if isBuiltInCommandInList {
-				fmt.Println(firstArg + " is a shell builtin")
-			} else if path, ok := isCommandExecutableInPath(firstArg); ok {
-				fmt.Println(firstArg + " is " + path)
-			} else {
-				fmt.Println(firstArg + ": not found")
-			}
-
-		} else {
-
-			err := executeCommand(cmd, args)
-			if err != nil {
-				fmt.Println(cmd + ": command not found")
-			}
-
-		}
+		handleCommands(cmd, args)
 	}
 }
 
@@ -83,12 +46,61 @@ func isCommandExecutableInPath(cmd string) (string, bool) {
 	return path, true
 }
 
-func executeCommand(cmd string, args []string) error {
+func executeCommand(cmd string, args []string) {
 	c := exec.Command(cmd, args...)
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 
 	err := c.Run()
-	return err
+	if err != nil {
+		fmt.Println(cmd + ": command not found")
+	}
+}
+
+func handleCommands(cmd string, args []string) {
+	switch cmd {
+	case "exit":
+
+		executeExitCommand(cmd, args)
+	case "echo":
+		executeEchoCommand(cmd, args)
+	case "type":
+		executeTypeCommand(cmd, args)
+	default:
+		executeCommand(cmd, args)
+	}
+}
+
+func executeExitCommand(cmd string, args []string) {
+	var firstArg string
+	if len(args) != 0 {
+		firstArg = args[0]
+	} else {
+		firstArg = ""
+	}
+
+	if firstArg == "0" {
+		os.Exit(0)
+	}
+	if firstArg == "1" {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func executeEchoCommand(cmd string, args []string) {
+	fmt.Print(strings.Join(args, " ") + "\n")
+}
+
+func executeTypeCommand(cmd string, args []string) {
+	var firstArg string = args[0]
+	isBuiltInCommandInList := slices.Contains(builtIncommandList, firstArg)
+	if isBuiltInCommandInList {
+		fmt.Println(firstArg + " is a shell builtin")
+	} else if path, ok := isCommandExecutableInPath(firstArg); ok {
+		fmt.Println(firstArg + " is " + path)
+	} else {
+		fmt.Println(firstArg + ": not found")
+	}
 }
